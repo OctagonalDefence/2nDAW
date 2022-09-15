2e5c2b712aef2e38dac534c2bb051665cf918435
OctagonalDefence <eromero110@boscdelacoma.cat>
2022-09-15 17:42:53 +0200

    h

---
+++ variablesprimitius.php
@@ -0,0 +1,32 @@
+<?php
+$i = 12;
+$c =  1.4;
+$s = "hola hola";
+$b = true;
+
+$tipus_de_i = gettype( $i );
+$tipus_de_c = gettype( $c );
+$tipus_de_s = gettype( $s );
+$tipus_de_b = gettype( $b );
+echo "La variable \$i 
+      conté el valor $i 
+	  i és del tipus $tipus_de_i
+      ";
+      echo '<xmp></xmp>';
+     echo  "La variable \$c 
+      conté el valor $c 
+	  i és del tipus $tipus_de_c
+      ";
+      echo '<xmp></xmp>';
+      echo "La variable \$s 
+      conté el valor $s 
+	  i és del tipus $tipus_de_s
+      ";
+      echo '<xmp></xmp>';
+      echo "La variable \$b 
+      conté el valor $b 
+	  i és del tipus $tipus_de_b"
+     ;
+
+
+?>
